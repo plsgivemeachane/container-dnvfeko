1871182b24029c211b61068846404ef57e752831
plsgivemeachane <100744636+plsgivemeachane@users.noreply.github.com>
2023-12-02 21:51:45 +0700

    Update entry.sh

--- src/entry.sh
+++ src/entry.sh
@@ -1,7 +1,2 @@
 apt-get install zip unzip
-curl -s "https://get.sdkman.io" | bash
-source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk version
-sdk install java
-wget https://github.com/Rudolf-Barbu/Ward/releases/download/v1.8.8/ward-1.8.8.jar
-java -jar ward-1.8.8.jar
+python3 -m http.server 80
