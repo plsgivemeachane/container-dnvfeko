b1e8538842038adb6f4bbfaa1269a14f2ae02777
plsgivemeachane <100744636+plsgivemeachane@users.noreply.github.com>
2023-12-02 13:03:01 +0700

    Create entry.sh

---
+++ src/entry.sh
@@ -0,0 +1,2 @@
+wget -O /tmp/netdata-kickstart.sh https://my-netdata.io/kickstart.sh && sh /tmp/netdata-kickstart.sh --nightly-channel --claim-token 0RM7e4jBSaHfNm3en_z4qIfvUS1Qf_pj5Xfr-XnfPNqrceW9kyeT_XpQt1t2gdpC_wAUU0zR2ZMCy8VC1wrT1T_n5eGu1WaYH4wDQ0aSIheoU22BX1nrK0S3TQXPSmQllA3tsq4 --claim-rooms 4c36f195-7d92-41b8-852a-784f1d96aecd --claim-url https://app.netdata.cloud
+python3 -m http.server 80
