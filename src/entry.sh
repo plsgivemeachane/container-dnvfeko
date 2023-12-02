3348ca39877eccb88dcb4156a39c5ac807724bdf
plsgivemeachane <100744636+plsgivemeachane@users.noreply.github.com>
2023-12-02 13:21:17 +0700

    Update entry.sh

--- src/entry.sh
+++ src/entry.sh
@@ -1,2 +1,2 @@
-wget -O /tmp/netdata-kickstart.sh https://my-netdata.io/kickstart.sh && sh /tmp/netdata-kickstart.sh --stable-channel --claim-token OxS_1elmhmhp-f7nsU3deXFgehdgeZWcw4LeICJ0r_bKacbTTcMpqMHxa6aha2O7yFsKNUWoVxNiXr_mNlkEJjWJVsnZ69nmMqpoaZQjIYbHlq5oJuaI6G6nrEs5YV3ZGrWewLU --claim-rooms 592bca71-2a3e-4c5c-998a-aa74cd9a3aa5 --claim-url https://app.netdata.cloud
+sudo wget -O /tmp/netdata-kickstart.sh https://my-netdata.io/kickstart.sh && sh /tmp/netdata-kickstart.sh --stable-channel --claim-token OxS_1elmhmhp-f7nsU3deXFgehdgeZWcw4LeICJ0r_bKacbTTcMpqMHxa6aha2O7yFsKNUWoVxNiXr_mNlkEJjWJVsnZ69nmMqpoaZQjIYbHlq5oJuaI6G6nrEs5YV3ZGrWewLU --claim-rooms 592bca71-2a3e-4c5c-998a-aa74cd9a3aa5 --claim-url https://app.netdata.cloud
 python3 -m http.server 80
