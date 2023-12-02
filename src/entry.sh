a34904866d108c759eb7f63847c4d50311db9196
plsgivemeachane <100744636+plsgivemeachane@users.noreply.github.com>
2023-12-02 13:34:20 +0700

    Update entry.sh

--- src/entry.sh
+++ src/entry.sh
@@ -1,2 +1,6 @@
-wget -O /tmp/netdata-kickstart.sh https://my-netdata.io/kickstart.sh && sh /tmp/netdata-kickstart.sh --stable-channel --claim-token OxS_1elmhmhp-f7nsU3deXFgehdgeZWcw4LeICJ0r_bKacbTTcMpqMHxa6aha2O7yFsKNUWoVxNiXr_mNlkEJjWJVsnZ69nmMqpoaZQjIYbHlq5oJuaI6G6nrEs5YV3ZGrWewLU --claim-rooms 592bca71-2a3e-4c5c-998a-aa74cd9a3aa5 --claim-url https://app.netdata.cloud &
-python3 -m http.server 80
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk version
+sdk install java
+wget https://github.com/Rudolf-Barbu/Ward/releases/download/v1.8.8/ward-1.8.8.jar
+java -jar ward-1.8.8.jar
